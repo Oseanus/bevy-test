4b6945bc8ba4c9f8a59cb991ac45a622c864d84c
Oliver Ellis <ellis.oliver@gmail.com>
2024-07-20 22:13:12 +0200

    Added first components

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,21 @@ fn hello_world() {
     println!("Hello World!");
 }
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Oliver Ellis".to_string())));
+    commands.spawn((Person, Name("Patrick Dehning".to_string())));
+    commands.spawn((Person, Name("Arne Eckel".to_string())));
+}
+
 fn main() {
     App::new()
+        .add_systems(Startup, add_people)
         .add_systems(Update, hello_world)
         .run();
 }
