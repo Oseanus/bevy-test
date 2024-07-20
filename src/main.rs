219aef808eb5228ed13f2eb6adc47b65e170483f
Oliver Ellis <ellis.oliver@gmail.com>
2024-07-20 22:51:00 +0200

    Added a first resource

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,17 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Arne Eckel".to_string())));
 }
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 // A system to query people created at startup schedule
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}", name.0);
+        }
     }
 }
-
 
 // A mutable system for changing attibutes of certain components
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
@@ -42,7 +46,8 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         // Systems are usually running parallel per scheduling level but sets of systems can be chained to a single task
-        app.add_systems(Startup, add_people)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
             .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
 
     }
